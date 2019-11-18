5470f1590497417f4feb24a7ad1c4a75b34ac308
Manoel Tavares <36523015+Manjor@users.noreply.github.com>
2019-11-18 10:48:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 8080
 
 const app = express();
 
@@ -28,4 +29,4 @@ io.on('connection', socket => {
 
 })
 
-server.listen(3000)
+server.listen(port)
